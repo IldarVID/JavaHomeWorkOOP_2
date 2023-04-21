24ce4172dc8b9501bb395fd99616d1e118025183
VID Neto <dello5@yandex.ru>
2023-04-21 12:48:41 +0300

    Initial commit

---
+++ src/main/java/ru/netology/services/Radio.java
@@ -0,0 +1,74 @@
+package ru.netology.services;
+
+public class Radio {
+
+    public int currentStation;
+    public int currentVolume;
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentStation(int newCurrentStation) {
+        if (newCurrentStation < 0) {
+            return;
+        }
+        if (newCurrentStation > 9) {
+            return;
+        }
+        currentStation = newCurrentStation;
+    }
+
+    public void nextStation() {
+        if (currentStation < 9) {
+            currentStation = currentStation + 1;
+        } else {
+            currentStation = 0;
+        }
+
+    }
+
+    public void prevStation() {
+
+        if (currentStation <= 9) {
+            currentStation = currentStation - 1;
+        }
+//        if (currentStation > 9) {
+//           return;
+//        }
+        if (currentStation < 0) {
+            currentStation = 9;
+        }
+    }
+
+    public void setCurrentVolume(int newCurrentVolume) {
+        if (newCurrentVolume < 0) {
+            return;
+        }
+        if (newCurrentVolume > 100) {
+            return;
+        }
+        currentVolume = newCurrentVolume;
+    }
+
+    public void increaseVolume() {
+        if (currentVolume < 100) {
+            currentVolume = currentVolume + 1;
+        }
+
+    }
+
+    public void turnDownVolume() {
+        if (currentVolume <= 100) {
+            currentVolume = currentVolume - 1;
+        }
+        if (currentVolume < 0) {
+            currentVolume = 0;
+        }
+    }
+
+}
